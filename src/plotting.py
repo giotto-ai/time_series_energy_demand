5b9d71c206de1777d2aab3e44e5f620b0c025a33
Colin Kälin <colinkalin@colins-mbp.lan>
2020-01-17 16:31:17 +0100

    add updates

--- src/plotting.py
+++ src/plotting.py
@@ -2,7 +2,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 
-def plot_time_series(df, y_columns, names=['trace_1', 'trace_2'], title='', x_axis_title='', y_axis_titles = ['', '']):
+def plot_time_series_2_axes(df, y_columns, names=['trace_1', 'trace_2'], title='', x_axis_title='', y_axis_titles = ['', '']):
     """Plot up to two time series with different y-axes.
     
     Parameters
@@ -57,5 +57,49 @@ def plot_time_series(df, y_columns, names=['trace_1', 'trace_2'], title='', x_ax
     
     if len(y_columns)==2:
         fig.update_yaxes(title_text=y_axis_titles[1], secondary_y=True)
+
+    return fig
+
+
+def plot_time_series(df, y_columns, title='', x_axis_title='', y_axis_title = ''):
+    """Plot different time series with the same y-axis.
+    
+    Parameters
+    ----------
+    df : pandas DataFrame
+        The dataframe with the data to plot (incl. the x-values as an index)
+    y_columns : list
+        The columns of the dataframe to use. Maximal length = 2
+    title : str, optional, default: ''
+        Title to put above the plot
+    x_axis_title : str, optional, default: ''
+        Title of the x-axis
+    y_axis_titles : list, optional, default: ['', '']
+        List of y_axis_titles to use. Maximal length = 2
+    """
+    x = df.index
+    
+    fig = make_subplots(specs=[[{"secondary_y": False}]])
+
+    for i in y_columns:
+        # Add traces
+        fig.add_trace(
+            go.Scatter(x=x, y=df[i].values, name=i),
+        )
+
+    # Add figure title
+    fig.update_layout(
+        title={
+        'text':title,
+        'y':0.9,
+        'x':0.5,
+        }
+    )
+
+    # Set x-axis title
+    fig.update_xaxes(title_text=x_axis_title)
+
+    # Set y-axes titles
+    fig.update_yaxes(title_text=y_axis_title)
 
     return fig
